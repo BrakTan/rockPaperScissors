5801e526fa23699abf049570d02b27e65f2212fa
Tanguy Leconte <taleconte@gmail.com>
2023-03-03 12:14:46 +0100

    Create logic for the game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber){
         case 1:
             return "rock";
@@ -16,7 +16,40 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
-    let playerChoice = prompt("Choose between Rock, Paper or Scissors.").toLowerCase();
+    const playerChoice = prompt("Choose between Rock, Paper or Scissors.").toLowerCase();
     return playerChoice;
 }
 
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
+
+if(playerChoice === computerChoice){
+    console.log("Draw !!");
+}
+else if(playerChoice === "rock"){
+    if(computerChoice === "paper"){
+        console.log("You Lose ! Paper beats Rock !");
+    }
+    else if(computerChoice === "scissors"){
+        console.log("You Win ! Rock beats Scissors !");
+    }
+}
+else if(playerChoice === "paper"){
+    if(computerChoice === "scissors"){
+        console.log("You Lose ! Scissors beat Paper !");
+    }
+    else if(computerChoice === "rock"){
+        console.log("You Win ! Paper beats Rock !");
+    }
+}
+else if(playerChoice === "scissors"){
+    if(computerChoice === "rock"){
+        console.log("You Lose ! Scissors beat Rock !");
+    }
+    else if(computerChoice === "paper"){
+        console.log("You Win ! Scissors beat Paper !");
+    }
+}
+else{
+    console.log("Did you write it well ?");
+}
