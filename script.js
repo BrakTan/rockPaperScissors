0a04b613e5ac9257a1b378689a35dd6261cbcaea
Tanguy Leconte <taleconte@gmail.com>
2023-03-03 11:59:26 +0100

    Create the logic for computer choice

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            return "I don't know...";
+    }
+}
+
